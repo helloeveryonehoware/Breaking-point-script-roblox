c6b582206186857d0d3f987408ab742ac4ddf8bb
2ez2playroblox <113609022+helloeveryonehoware@users.noreply.github.com>
2022-12-12 11:12:56 -0600

    Update Script I'm making.lua

--- Script I'm making.lua
+++ Script I'm making.lua
@@ -1,6 +1,6 @@
 --Gui
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("Best hub", "Ocean")
+local Window = Library.CreateLib("loanoforeve's bp script", "Ocean")
 
 --Main
 local Autofarmstuff = Window:NewTab("Autofarm")
