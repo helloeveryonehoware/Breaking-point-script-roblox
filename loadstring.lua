31f7c22a0eaa6d4814e2a35e9b640e952885f217
2ez2playroblox <113609022+helloeveryonehoware@users.noreply.github.com>
2022-12-15 11:10:46 -0600

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/helloeveryonehoware/Breaking-point-script-roblox/main/Script%20I'm%20making.lua")())
+loadstring(game:HttpGet('https://raw.githubusercontent.com/helloeveryonehoware/Breaking-point-script-roblox/main/loanoforevebp.lua'))()
