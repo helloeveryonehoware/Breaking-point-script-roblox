9fe342421ea8211484e90723ded746160dfdeb0b
2ez2playroblox <113609022+helloeveryonehoware@users.noreply.github.com>
2023-08-26 20:37:43 -0500

    adding a actual key sys

--- loanoforevebp.lua
+++ loanoforevebp.lua
@@ -11,15 +11,15 @@ local Window = Rayfield:CreateWindow({
        FileName = "loanoforevebp"
     },
     Discord = {
-       Enabled = false,
-       Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
+       Enabled = true,
+       Invite = "Q3pqBxD7G2", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
        RememberJoins = true -- Set this to false to make them join the discord every time they load it up
     },
 	KeySystem = true,
     KeySettings = {
        Title = "loanoforevebp key sys",
        Subtitle = "Key System",
-       Note = "No method of obtaining the key is provided",
+       Note = "get the key in the discord server https://discord.gg/Q3pqBxD7G2 or https://link-target.net/941382/key",
        FileName = "loanoforevesbpkey", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
        SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
        GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
